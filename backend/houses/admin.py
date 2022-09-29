594fabf16b5f09489c04a32a476990365032c3c8
LDG - DGit <hi-ldg@dgit.co>
2022-09-29 23:11:20 +0900

    create user app

--- backend/houses/admin.py
+++ backend/houses/admin.py
@@ -4,16 +4,6 @@ from .models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "price",
-        "address",
-        "pets_allowed"
-    )
+    list_display = ("name", "price", "address", "pets_allowed")
 
-    list_filter = (
-        "price",
-        "pets_allowed"
-    )
-
-    search_fields = ("address", )
+    list_filter = ("price", "pets_allowed")
--- backend/houses/models.py
+++ backend/houses/models.py
@@ -10,7 +10,11 @@ class House(models.Model):
     price = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=200)
-    pets_allowed = models.BooleanField(default=False)
+    pets_allowed = models.BooleanField(
+        default=False,
+        help_text="Does this house allows pets?",
+        verbose_name="Pets allowed?",
+    )
 
     def __str__(self):
         return self.name
